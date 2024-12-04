d68b8697be4ddf1c17b5110d447016a1b250230c
Gabriela Lizbeth Alvarado Martinez <gabrielalizbethalvaradomartinez@MacBook-Pro-de-Gabriela.local>
2024-12-04 11:06:13 -0600

    Se agrega un marcador al mapa.

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild, ElementRef, OnDestroy } from '@angular/core';
+import { LngLat, Map, Marker } from 'mapbox-gl'; 
 
 @Component({
   templateUrl: './markers-page.component.html',
   styleUrls: ['./markers-page.component.css']
 })
-export class MarkersPageComponent {
+export class MarkersPageComponent implements AfterViewInit, OnDestroy {
+
+  @ViewChild('map') divMap?: ElementRef;
+
+  public map?: Map;
+  public currentLngLat: LngLat = new LngLat(-100.99858792105704, 22.151313492980307);
+  
+  ngAfterViewInit(): void {
+    if ( !this.divMap ) throw 'El elemento HTML no fue encontrado';
+
+    this.map = new Map({
+      container: this.divMap?.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.currentLngLat, // starting position [lng, lat]
+      zoom: 13,
+    });
+
+    const markerHtml = document.createElement('div');
+    markerHtml.innerHTML = 'Gabriela A';
+
+    const marker = new Marker({
+      //color: 'red',
+      element: markerHtml
+    })
+      .setLngLat( this.currentLngLat )
+      .addTo( this.map );
+  }
+
+  ngOnDestroy(): void {
+    this.map?.remove();
+  }
 
 }
