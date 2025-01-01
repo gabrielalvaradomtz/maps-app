782f7e9b8125b8050e50adc1fa3834fa816db645
Gabriela Lizbeth Alvarado Martinez <gabrielalizbethalvaradomartinez@MacBook-Pro-de-Gabriela.local>
2025-01-01 17:19:54 -0600

    Se agrega mini mapa a propiedades.

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Map, Marker } from 'mapbox-gl';
 
 @Component({
-  selector: 'app-mini-map',
+  selector: 'map-mini-map',
   templateUrl: './mini-map.component.html',
   styleUrls: ['./mini-map.component.css']
 })
 export class MiniMapComponent {
+
+  @Input() lngLat?: [number, number];
+  @ViewChild('map') divMap?: ElementRef;
+
+  ngAfterViewInit() {
+    
+    if ( !this.divMap?.nativeElement ) throw "Map Div not found";
+
+    if ( !this.lngLat ) throw "LngLat can´t be null";
+
+    const map = new Map({
+      container: this.divMap.nativeElement, 
+      style: 'mapbox://styles/mapbox/streets-v12', 
+      center: this.lngLat, 
+      zoom: 15,
+      interactive: false
+    });
+
+    new Marker()
+      .setLngLat( this.lngLat )
+      .addTo( map )
+    
+  }
 
 }
