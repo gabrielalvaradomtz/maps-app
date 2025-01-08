2af78fedc25f4fdba95485f4eec3aae8766d2f1b
Gabriela Lizbeth Alvarado Martinez <gabrielalizbethalvaradomartinez@MacBook-Pro-de-Gabriela.local>
2025-01-07 21:04:46 -0600

    Se crea componente contador standalone.

--- src/app/alone/pages/alone-page/alone-page.component.ts
+++ src/app/alone/pages/alone-page/alone-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CounterAloneComponent } from "../../components/counter-alone/counter-alone.component";
 
 @Component({
   standalone: true,
+  imports: [CounterAloneComponent],
   templateUrl: './alone-page.component.html',
-  styleUrls: ['./alone-page.component.css']
+  styleUrls: ['./alone-page.component.css'],
 })
 export class AlonePageComponent {
 
